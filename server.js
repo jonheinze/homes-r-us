f67fa55a32eb64d06acf63652431034246cec3b4
Jonathan Heinze <jonathan@weareenvoy.com>
2016-06-17 15:33:33 -0700

    Change port

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log("Server running on port 3000");
+app.listen(80, function () {
+  console.log("Server running on port 80");
 });
